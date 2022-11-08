c0eb216a266e73314483e40f709d610966c64d2a
Peter Wang <40186116+PeteMango@users.noreply.github.com>
2022-11-08 17:20:30 -0500

    made header file for function

---
+++ arduinoFiles/buttonClickTest/testingAccelerometer/functions.h
@@ -0,0 +1,13 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+void checkSettings();
+void loop();
+void findIp(int time1);
+void showIP();
+void establishConnection(String command, int timeOut);
+void wifi_init();
+void sendData(String server1);
+void sendToServer();
+
+#endif
