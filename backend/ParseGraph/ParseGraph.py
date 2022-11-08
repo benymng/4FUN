4d7ca33262e20585824e6fa349a70560cb91edbf
Alex <a3zhu@uwaterloo.ca>
2022-11-08 17:21:30 -0500

    parsed html and graphed accelerometer data

---
+++ backend/ParseGraph/ParseGraph.py
@@ -0,0 +1,42 @@
+# author: alex zhu and ben ng
+# date: nov 8, 2022
+# parses from site and graphs data
+
+import os
+from bs4 import BeautifulSoup
+import requests
+import numpy as np
+from matplotlib import pyplot as plt
+
+# number of data points
+LENGTH = 5
+accel = []
+
+def main():
+    getData()
+    print(accel)
+
+    # some sample code
+    x = np.arange(0, LENGTH)
+    y = np.array(accel)
+
+    plt.title("Accelerometer data") 
+    plt.xlabel("Time") 
+    plt.ylabel("Acceleration") 
+    plt.plot(x,y) 
+    plt.show()
+
+def getData():
+    url = "http://127.0.0.1:5500/index.html"
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    text = soup.find_all('h2')
+    for one in text:
+        item = one.get_text()
+        accel.append(float(item))
+        print(item)
+
+# def graphData():
+
+if __name__ == '__main__':
+    main()
