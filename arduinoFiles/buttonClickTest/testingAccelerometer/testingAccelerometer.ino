29cb14c9a4bb474f5e14b20e9627f7d2077fd9d7
Emma Huang <emmahuang.ty@gmail.com>
2022-11-08 16:55:42 -0500

    edited accelerometer logic

--- arduinoFiles/buttonClickTest/testingAccelerometer/testingAccelerometer.ino
+++ arduinoFiles/buttonClickTest/testingAccelerometer/testingAccelerometer.ino
@@ -1,14 +1,14 @@
 #include<SoftwareSerial.h>
-#include <Wire.h>
-#include <MPU6050.h>       
+// #include <Wire.h>
+// #include <MPU6050.h>       
 SoftwareSerial comm(10, 11); //setting Tx and Rx pins
 
 String server = ""; //variable for sending data to webpage
 boolean No_IP = false; //variable to check for ip Address
 String IP = ""; //variable to store ip Address
 char temp1 = '0';
-const String SSID = "emma";
-const String PWD = "emmaiscool";
+const String SSID = "Norm";
+const String PWD = "normanch";
 
 int a = 0;
 int b = 0;
@@ -21,13 +21,13 @@ int buttonState = 0;        // current state of the button
 int lastButtonState = 0;    // previous state of the button
 
 // MPU6050
-MPU6050 mpu;
+// MPU6050 mpu;
 
 String str1 = "Button push counter here"; //String to display on webpage
-String accelOutput = "none";
+// String accelOutput = "none";
 String str2 = "<p>Data Received Successfully.....</p>"; //another string to display on webpage
 
-void checkSettings(); 
+// void checkSettings(); 
 
 void setup()
 {
@@ -36,46 +36,46 @@ void setup()
   wifi_init();
   Serial.println("System Ready..");
   // detect if the MPU6050 accelerometer is connected
-  while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G)) {
-    Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
-  }
+  // while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G)) {
+  //   Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
+  // }
 
-  // check the setting to see if its properly initialized
-  checkSettings();    
+  // // check the setting to see if its properly initialized
+  // checkSettings();    
 }
 
 // check the settings of the accelerometer
-void checkSettings() {
-  Serial.println();
-  Serial.print(" * Sleep Mode:            ");
-  Serial.println(mpu.getSleepEnabled() ? "Enabled" : "Disabled");
-  
-  Serial.print(" * Clock Source:          ");
-  switch(mpu.getClockSource()) {
-    case MPU6050_CLOCK_KEEP_RESET:     Serial.println("Stops the clock and keeps the timing generator in reset"); break;
-    case MPU6050_CLOCK_EXTERNAL_19MHZ: Serial.println("PLL with external 19.2MHz reference"); break;
-    case MPU6050_CLOCK_EXTERNAL_32KHZ: Serial.println("PLL with external 32.768kHz reference"); break;
-    case MPU6050_CLOCK_PLL_ZGYRO:      Serial.println("PLL with Z axis gyroscope reference"); break;
-    case MPU6050_CLOCK_PLL_YGYRO:      Serial.println("PLL with Y axis gyroscope reference"); break;
-    case MPU6050_CLOCK_PLL_XGYRO:      Serial.println("PLL with X axis gyroscope reference"); break;
-    case MPU6050_CLOCK_INTERNAL_8MHZ:  Serial.println("Internal 8MHz oscillator"); break;
-  }
-  Serial.print(" * Accelerometer:         ");
-  switch(mpu.getRange()) {
-    case MPU6050_RANGE_16G:            Serial.println("+/- 16 g"); break;
-    case MPU6050_RANGE_8G:             Serial.println("+/- 8 g"); break;
-    case MPU6050_RANGE_4G:             Serial.println("+/- 4 g"); break;
-    case MPU6050_RANGE_2G:             Serial.println("+/- 2 g"); break;
-  }  
-  Serial.print(" * Accelerometer offsets: ");
-  Serial.print(mpu.getAccelOffsetX());
-  Serial.print(" / ");
-  Serial.print(mpu.getAccelOffsetY());
-  Serial.print(" / ");
-  Serial.println(mpu.getAccelOffsetZ());
-  
-  Serial.println();
-}
+// void checkSettings() {
+//   Serial.println();
+//   Serial.print(" * Sleep Mode:            ");
+//   Serial.println(mpu.getSleepEnabled() ? "Enabled" : "Disabled");
+
+//   Serial.print(" * Clock Source:          ");
+//   switch(mpu.getClockSource()) {
+//     case MPU6050_CLOCK_KEEP_RESET:     Serial.println("Stops the clock and keeps the timing generator in reset"); break;
+//     case MPU6050_CLOCK_EXTERNAL_19MHZ: Serial.println("PLL with external 19.2MHz reference"); break;
+//     case MPU6050_CLOCK_EXTERNAL_32KHZ: Serial.println("PLL with external 32.768kHz reference"); break;
+//     case MPU6050_CLOCK_PLL_ZGYRO:      Serial.println("PLL with Z axis gyroscope reference"); break;
+//     case MPU6050_CLOCK_PLL_YGYRO:      Serial.println("PLL with Y axis gyroscope reference"); break;
+//     case MPU6050_CLOCK_PLL_XGYRO:      Serial.println("PLL with X axis gyroscope reference"); break;
+//     case MPU6050_CLOCK_INTERNAL_8MHZ:  Serial.println("Internal 8MHz oscillator"); break;
+//   }
+//   Serial.print(" * Accelerometer:         ");
+//   switch(mpu.getRange()) {
+//     case MPU6050_RANGE_16G:            Serial.println("+/- 16 g"); break;
+//     case MPU6050_RANGE_8G:             Serial.println("+/- 8 g"); break;
+//     case MPU6050_RANGE_4G:             Serial.println("+/- 4 g"); break;
+//     case MPU6050_RANGE_2G:             Serial.println("+/- 2 g"); break;
+//   }  
+//   Serial.print(" * Accelerometer offsets: ");
+//   Serial.print(mpu.getAccelOffsetX());
+//   Serial.print(" / ");
+//   Serial.print(mpu.getAccelOffsetY());
+//   Serial.print(" / ");
+//   Serial.println(mpu.getAccelOffsetZ());
+
+//   Serial.println();
+// }
 
 void loop()
 {
@@ -83,8 +83,9 @@ void loop()
   // Serial.println("Refresh Page");
   // while (1)
   // {
-    Serial.println("Ready to go");
-    b++;
+  //   b++;
+  Serial.write(1);
+  Serial.print(comm.read());
     if (comm.availableForWrite())
     {
           Serial.println("Here");
@@ -95,34 +96,26 @@ void loop()
         sendToServer();
         Serial.println("Finished");
         delay(1000);
-      // }
-    }
-  // }
+      }
 
-  Vector rawAccel = mpu.readRawAccel();
-  accelOutput = " Yraw = ";
-  accelOutput.concat(rawAccel.YAxis);
-  comm.println(accelOutput);
 
-  delay(10000);
-
-  // buttonState = digitalRead(buttonPin);
-  // // compare the buttonState to its previous state
-  // // Serial.println(buttonState);
-  // if(buttonState != lastButtonState) {
-  //   lastButtonState = buttonState;
-  //   buttonPushCounter += 1;
-  //   Serial.println(buttonPushCounter);
-  //     if(buttonState == HIGH) {
-  //       Serial.println ("turned LED off");
-  //       digitalWrite(ledPin, LOW);
-  //     }
-  //     else {
-  //       Serial.println("turned LED on");
-  //       digitalWrite(ledPin, HIGH);
-  //     }
-  //     delay(50);
-  // }
+  buttonState = digitalRead(buttonPin);
+  // compare the buttonState to its previous state
+  // Serial.println(buttonState);
+  if(buttonState != lastButtonState) {
+    lastButtonState = buttonState;
+    buttonPushCounter += 1;
+    Serial.println(buttonPushCounter);
+      if(buttonState == HIGH) {
+        Serial.println ("turned LED off");
+        digitalWrite(ledPin, LOW);
+      }
+      else {
+        Serial.println("turned LED on");
+        digitalWrite(ledPin, HIGH);
+      }
+      delay(50);
+  }
 }
 
 
@@ -254,9 +247,9 @@ void sendToServer()//send data to webpage
 {
   server = "<h1>HI</h1>";
   sendData(server);
-  server = accelOutput;
+  server = String(buttonPushCounter);
   server += str2;
   sendData(server);
-  delay(1000);
+  delay(5000);
   comm.println("AT+CIPCLOSE=0");
 }
