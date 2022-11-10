7357108816e559dd7b50c1d0f3b3d935da1581f6
Alex <a3zhu@uwaterloo.ca>
2022-11-10 10:26:52 -0500

    combined parse and graph code

--- backend/ParseGraph/ParseGraphDatabase.py
+++ backend/ParseGraph/ParseGraphDatabase.py
@@ -1,12 +1,16 @@
 # author: alex zhu and ben ng
-# date: nov 8, 2022
-# parses from site and graphs data
+# date: nov 10, 2022
+# parses from site and graphs data: 1 of 2 files for prototype
 
+from pymongo import MongoClient
+from dotenv import load_dotenv
 import os
 from bs4 import BeautifulSoup
 import requests
+
 import numpy as np
 from matplotlib import pyplot as plt
+# use source env/bin/activate to activate environment
 
 # number of data points
 LENGTH = 5
@@ -14,6 +18,11 @@ accel = []
 
 def main():
     getData()
+    
+    # database part
+    load_dotenv()
+
+    # graph part
     print(accel)
 
     # some sample code
@@ -26,6 +35,22 @@ def main():
     plt.plot(x,y) 
     plt.show()
 
+def get_database():
+    CONNECTION_STRING = (f'mongodb+srv://Ben:{os.getenv("MONGO_PASSWORD")}@cluster0.qtjn2.mongodb.net/?retryWrites=true&w=majority')
+    client = MongoClient(CONNECTION_STRING)
+    # print(client.list_database_names())
+    return client['4FUN']
+
+def insert_into_database(item):
+    dbname = get_database()
+    myCol = dbname["newTest"]
+
+    # collection_name = dbname["testing"]
+
+    x = myCol.insert_one(item)
+    print(x.inserted_id)
+    # collection_name.insert_one(item)
+
 def getData():
     url = "http://127.0.0.1:5500/backend/ParseGraph/index.html"
     page = requests.get(url)
@@ -35,8 +60,7 @@ def getData():
         item = one.get_text()
         accel.append(float(item))
         print(item)
-
-# def graphData():
-
+        
+    
 if __name__ == '__main__':
     main()
