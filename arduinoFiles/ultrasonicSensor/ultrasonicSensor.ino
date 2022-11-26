4460bcc57414b5c872de2a2563e20d6ee2fb1856
Peter Wang <40186116+PeteMango@users.noreply.github.com>
2022-11-26 13:40:38 -0500

    updated ultrasonic code with break

--- arduinoFiles/ultrasonicSensor/ultrasonicSensor.ino
+++ arduinoFiles/ultrasonicSensor/ultrasonicSensor.ino
@@ -1,16 +1,15 @@
-#define echoPin 7 // attach pin D2 Arduino to pin Echo of HC-SR04
-#define trigPin 5 //attach pin D3 Arduino to pin Trig of HC-SR04
+#define echoPin 7 
+#define trigPin 5 
 
 bool repRn = false;
 long duration=0;
-int distance=0, countRep=0; 
+int distance=0, countRep=0, countTime=0;
 
 void setup() {
   pinMode(trigPin, OUTPUT); 
   pinMode(echoPin, INPUT); 
   Serial.begin(9600);
   Serial.println("Ultrasonic Sensor HC-SR04 Test"); 
-  Serial.println("with Arduino UNO R3");
 }
 void loop() {
   digitalWrite(trigPin, LOW); 
@@ -19,18 +18,27 @@ void loop() {
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-  
+
   duration = pulseIn(echoPin, HIGH);
   distance = duration * 0.034 / 2; 
 
   if(distance < 5) {
     repRn = true;
+    countTime++;
   }
   if(distance > 10 && repRn == true) {
     countRep++;
     repRn = false;
+    countTime=0;
   }
 
+  if(countTime > 5) {
+    Serial.println("You have completed your set!");
+    Serial.print("In this set, you did: ");
+    Serial.print(countRep);
+    Serial.println(" reps!");
+    exit(0);
+  }
   Serial.print("Distance: ");
   Serial.print(distance);
   Serial.println(" cm");
